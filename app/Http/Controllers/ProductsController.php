55fe17aca3c54e07319606a84e05f519f79ecbbb
vagrant <vagrant@homestead>
2019-10-05 13:19:00 +0000

    商品筛选-排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,9 +9,42 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
+        // 创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
 
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        // 判断是否有提交参数
+        if($search = $request->input('search', '')){
+            $like = '%' . $search . '%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
+            $builder->where(function($query) use ($like){
+                $query->where('title', 'like', $like)
+                      ->orWhere('description', 'like', $like)
+                      ->whereHas('skus', function($query) use ($like){
+                          $query->where('title', 'like', $like)
+                                ->orWhere('description', 'like', $like);
+                      });
+            });
+        }
 
-        return view('products.index', ['products' => $products]);
+        // 判断是否有提交order参数，如果有就赋值给$order变量 $order参数用来控制商品的排序规则
+        if($order = $request->input('order', '')){
+            // 是否以_asc或_desc结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order
+            ]
+        ]);
     }
 }
