8bbd093957e64c955d41b03784029718b79ced84
vagrant <vagrant@homestead>
2019-10-15 09:59:52 +0000

    mysql备份脚本

--- database/admin.sql
+++ database/admin.sql
@@ -1,62 +1,106 @@
--- ----------------------------
--- Records of admin_menu
--- ----------------------------
-INSERT INTO `admin_menu` VALUES ('1', '0', '1', '首页', 'fa-bar-chart', '/', null, null, '2019-10-15 15:14:20');
-INSERT INTO `admin_menu` VALUES ('2', '0', '6', '系统管理', 'fa-tasks', null, null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('3', '2', '7', '管理员', 'fa-users', 'auth/users', null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('4', '2', '8', '角色', 'fa-user', 'auth/roles', null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('5', '2', '9', '权限', 'fa-ban', 'auth/permissions', null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('6', '2', '10', '菜单', 'fa-bars', 'auth/menu', null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('7', '2', '11', '操作日志', 'fa-history', 'auth/logs', null, null, '2019-10-15 15:24:58');
-INSERT INTO `admin_menu` VALUES ('8', '0', '2', '用户管理', 'fa-users', '/users', null, '2019-10-15 15:16:13', '2019-10-15 15:24:57');
-INSERT INTO `admin_menu` VALUES ('9', '0', '4', '订单管理', 'fa-rmb', '/orders', null, '2019-10-15 15:18:11', '2019-10-15 15:24:57');
-INSERT INTO `admin_menu` VALUES ('10', '0', '3', '商品管理', 'fa-cubes', '/products', null, '2019-10-15 15:18:24', '2019-10-15 15:24:57');
-INSERT INTO `admin_menu` VALUES ('11', '0', '5', '优惠券管理', 'fa-tags', '/coupon_codes', null, '2019-10-15 15:18:59', '2019-10-15 15:24:57');
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: 192.168.10.10    Database: laravel_shop
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.18.04.1
 
--- ----------------------------
--- Records of admin_permissions
--- ----------------------------
-INSERT INTO `admin_permissions` VALUES ('1', 'All permission', '*', '', '*', null, null);
-INSERT INTO `admin_permissions` VALUES ('2', 'Dashboard', 'dashboard', 'GET', '/', null, null);
-INSERT INTO `admin_permissions` VALUES ('3', 'Login', 'auth.login', '', '/auth/login\r\n/auth/logout', null, null);
-INSERT INTO `admin_permissions` VALUES ('4', 'User setting', 'auth.setting', 'GET,PUT', '/auth/setting', null, null);
-INSERT INTO `admin_permissions` VALUES ('5', 'Auth management', 'auth.management', '', '/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs', null, null);
-INSERT INTO `admin_permissions` VALUES ('6', '用户管理', 'users', '', '/users*', '2019-10-15 15:25:47', '2019-10-15 15:25:47');
-INSERT INTO `admin_permissions` VALUES ('7', '商品管理', 'products', '', '/products*', '2019-10-15 15:26:22', '2019-10-15 15:26:22');
-INSERT INTO `admin_permissions` VALUES ('8', '优惠券管理', 'coupon_codes', '', '/coupon_codes*', '2019-10-15 15:27:34', '2019-10-15 15:27:34');
-INSERT INTO `admin_permissions` VALUES ('9', '订单管理', 'orders', '', '/orders*', '2019-10-15 15:28:14', '2019-10-15 15:28:14');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- ----------------------------
--- Records of admin_role_menu
--- ----------------------------
-INSERT INTO `admin_role_menu` VALUES ('1', '2', null, null);
+--
+-- Dumping data for table `admin_menu`
+--
 
--- ----------------------------
--- Records of admin_role_permissions
--- ----------------------------
-INSERT INTO `admin_role_permissions` VALUES ('1', '1', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '2', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '3', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '4', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '6', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '7', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '8', null, null);
-INSERT INTO `admin_role_permissions` VALUES ('2', '9', null, null);
+LOCK TABLES `admin_menu` WRITE;
+/*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-10-15 15:14:20'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-10-15 17:48:38'),(3,2,8,'管理员','fa-users','auth/users',NULL,NULL,'2019-10-15 17:48:38'),(4,2,9,'角色','fa-user','auth/roles',NULL,NULL,'2019-10-15 17:48:38'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-10-15 17:48:38'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-10-15 17:48:38'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-10-15 17:48:38'),(8,0,2,'用户管理','fa-users','/users',NULL,'2019-10-15 15:16:13','2019-10-15 15:24:57'),(9,0,5,'订单管理','fa-rmb','/orders',NULL,'2019-10-15 15:18:11','2019-10-15 17:48:38'),(10,0,4,'商品管理','fa-cubes','/products',NULL,'2019-10-15 15:18:24','2019-10-15 17:48:38'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes',NULL,'2019-10-15 15:18:59','2019-10-15 17:48:38'),(12,0,3,'商品分类','fa-bars','/categories',NULL,'2019-10-15 17:48:29','2019-10-15 17:48:38');
+/*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- ----------------------------
--- Records of admin_role_users
--- ----------------------------
-INSERT INTO `admin_role_users` VALUES ('1', '1', null, null);
-INSERT INTO `admin_role_users` VALUES ('2', '2', null, null);
+--
+-- Dumping data for table `admin_permissions`
+--
 
--- ----------------------------
--- Records of admin_roles
--- ----------------------------
-INSERT INTO `admin_roles` VALUES ('1', 'Administrator', 'administrator', '2019-10-15 14:48:48', '2019-10-15 14:48:48');
-INSERT INTO `admin_roles` VALUES ('2', '运营经理', 'operation', '2019-10-15 15:32:07', '2019-10-15 15:32:07');
+LOCK TABLES `admin_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
+INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'用户管理','users','','/users*','2019-10-15 15:25:47','2019-10-15 15:25:47'),(7,'商品管理','products','','/products*','2019-10-15 15:26:22','2019-10-15 15:26:22'),(8,'优惠券管理','coupon_codes','','/coupon_codes*','2019-10-15 15:27:34','2019-10-15 15:27:34'),(9,'订单管理','orders','','/orders*','2019-10-15 15:28:14','2019-10-15 15:28:14');
+/*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- ----------------------------
--- Records of admin_users
--- ----------------------------
-INSERT INTO `admin_users` VALUES ('1', 'admin', '$2y$10$hFC2weUuvx1xoFkIkM6b6./0Y.QSyHqfSuVhIV1bzMd42mzaBmNMe', 'Administrator', null, '16F9excD9NG8xXQSqMjbEgTrDsOug9e0Jx78lRDeLRVNUxRpaHpWMEQbX3ws', '2019-10-15 14:48:48', '2019-10-15 14:48:48');
-INSERT INTO `admin_users` VALUES ('2', 'operator', '$2y$10$3vhIatm7mi5fXxeDrYnr8eQmoZRg.qOirnxmFuWTUQc45rHed5MeC', 'Josh.manager', null, 'P4Q6KByR1e49sIH7TbRnGysZNa3J30xRTEbg3Wb9PW9zjACHk42kNQ9rv9E9', '2019-10-15 15:33:32', '2019-10-15 15:33:32');
+--
+-- Dumping data for table `admin_role_menu`
+--
+
+LOCK TABLES `admin_role_menu` WRITE;
+/*!40000 ALTER TABLE `admin_role_menu` DISABLE KEYS */;
+INSERT INTO `admin_role_menu` VALUES (1,2,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_role_permissions`
+--
+
+LOCK TABLES `admin_role_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_role_permissions` DISABLE KEYS */;
+INSERT INTO `admin_role_permissions` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL),(2,3,NULL,NULL),(2,4,NULL,NULL),(2,6,NULL,NULL),(2,7,NULL,NULL),(2,8,NULL,NULL),(2,9,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_role_users`
+--
+
+LOCK TABLES `admin_role_users` WRITE;
+/*!40000 ALTER TABLE `admin_role_users` DISABLE KEYS */;
+INSERT INTO `admin_role_users` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL);
+/*!40000 ALTER TABLE `admin_role_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_roles`
+--
+
+LOCK TABLES `admin_roles` WRITE;
+/*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
+INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2019-10-15 14:48:48','2019-10-15 14:48:48'),(2,'运营经理','operation','2019-10-15 15:32:07','2019-10-15 15:32:07');
+/*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_user_permissions`
+--
+
+LOCK TABLES `admin_user_permissions` WRITE;
+/*!40000 ALTER TABLE `admin_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_user_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+LOCK TABLES `admin_users` WRITE;
+/*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
+INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$hFC2weUuvx1xoFkIkM6b6./0Y.QSyHqfSuVhIV1bzMd42mzaBmNMe','Administrator',NULL,'16F9excD9NG8xXQSqMjbEgTrDsOug9e0Jx78lRDeLRVNUxRpaHpWMEQbX3ws','2019-10-15 14:48:48','2019-10-15 14:48:48'),(2,'operator','$2y$10$3vhIatm7mi5fXxeDrYnr8eQmoZRg.qOirnxmFuWTUQc45rHed5MeC','Josh.manager',NULL,'P4Q6KByR1e49sIH7TbRnGysZNa3J30xRTEbg3Wb9PW9zjACHk42kNQ9rv9E9','2019-10-15 15:33:32','2019-10-15 15:33:32');
+/*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-15  9:59:15
